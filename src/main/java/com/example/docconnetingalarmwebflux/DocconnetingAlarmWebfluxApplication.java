27b6e963a70e6972ec72538b6027231023e52c92
kyn1013 <yn112511@naver.com>
2025-04-26 13:52:14 +0900

    Init : 프로젝트 초기 세팅 (#1)

---
+++ src/main/java/com/example/docconnetingalarmwebflux/DocconnetingAlarmWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.example.docconnetingalarmwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocconnetingAlarmWebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocconnetingAlarmWebfluxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/docconnetingalarmwebflux/DocconnetingAlarmWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.docconnetingalarmwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocconnetingAlarmWebfluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
