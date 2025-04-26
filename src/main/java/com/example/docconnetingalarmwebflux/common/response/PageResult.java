8ddad29d5da343f2d5402596275df18968a3cced
kyn1013 <yn112511@naver.com>
2025-04-26 15:56:37 +0900

    Chore : 공통 모듈 추가 (#1)

---
+++ src/main/java/com/example/docconnetingalarmwebflux/common/response/PageResult.java
@@ -0,0 +1,18 @@
+package com.example.docconnetingalarmwebflux.common.response;
+
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class PageResult<T> {
+    private final List<T> content;
+    private final PageInfo pageInfo;
+
+    public PageResult(List<T> content, PageInfo pageInfo) {
+        this.content = content;
+        this.pageInfo = pageInfo;
+    }
+
+}
+
